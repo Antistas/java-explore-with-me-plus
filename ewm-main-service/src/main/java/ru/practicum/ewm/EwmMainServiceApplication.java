ab983d4c65e38d94c183acd52f12eaddb73a042a
Goggy <bigggrinz@gmail.com>
2026-07-24 21:19:18 +0500

    добавлен сервис статистики: БД, сервис, контроллер, необходимые ДТО, дополнен StatsClient его можно использовать для подключения к сервису статистики

--- ewm-main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApplication.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/EwmMainServiceApplication.java
@@ -2,10 +2,17 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.practicum.stats.client.StatsClient;
 
 @SpringBootApplication
 public class EwmMainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EwmMainServiceApplication.class, args);
+    }
+
+    @Bean
+    public StatsClient statsClient() {
+        return new StatsClient("http://stats-server:9090");
     }
 }
--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStats.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStats.java
@@ -9,10 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class EndpointHit {
-    private Long id;
+public class ViewStats {
     private String app;
     private String uri;
-    private String ip;
-    private String timestamp;
+    private Long hits;
 }
